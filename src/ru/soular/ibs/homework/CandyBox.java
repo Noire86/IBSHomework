96de0fb47865f69a021ec44c8f84b105fe83292d
Noire86 <gleb.iwanow2010@yandex.ru>
2021-12-24 16:36:32 +0300

    [Addition] Created optimization methods

--- src/ru/soular/ibs/homework/CandyBox.java
+++ src/ru/soular/ibs/homework/CandyBox.java
@@ -34,27 +34,97 @@ public class CandyBox implements IBoxable {
 
     @Override
     public void printWeight() {
-        int weight = 0;
-        for(AbstractCandy candy : array) {
-           weight += candy.getWeight();
-        }
-        System.out.println("Box weight is: " + weight);
+        System.out.println("Box weight is: " + getOverallWeight());
     }
 
     @Override
     public void printOverallCost() {
-        int cost = 0;
-        for (AbstractCandy candy : array) {
-            cost += candy.getCost();
-        }
-        System.out.println("Box overall cost is:" + cost);
+        System.out.println("Box overall cost is: " + getOverallCost());
     }
 
     @Override
     public void printContents() {
-        System.out.println("This box consists of:");
+        System.out.println("This box consists of: ");
         for (AbstractCandy candy : array) {
             System.out.println(candy.getName());
+        }
+    }
+
+    private int getOverallWeight() {
+        int weight = 0;
+        for (AbstractCandy candy : array) {
+            weight += candy.getWeight();
+        }
+        return weight;
+    }
+
+    private int getOverallCost() {
+        int cost = 0;
+        for (AbstractCandy candy : array) {
+            cost += candy.getCost();
+        }
+        return cost;
+    }
+
+    private AbstractCandy getMinWeightCandy() {
+        int minWeight = Integer.MAX_VALUE;
+
+        for (AbstractCandy candy : array) { // поиск самого меньшего веса
+            if (candy.getWeight() < minWeight) {
+                minWeight = candy.getWeight();
+            }
+        }
+
+        for (AbstractCandy candy : array) {//пробегаемся по листу и выбираем самый легкий обьект, возвращаем его
+            if (minWeight == candy.getWeight()) {
+                return candy;
+            }
+        }
+        return null;
+    }
+
+    private AbstractCandy getMinCostCandy() {
+        int minCost = Integer.MAX_VALUE;
+
+        for (AbstractCandy candy : array) { // поиск меньшей цены
+            if (candy.getCost() < minCost) {
+                minCost = candy.getCost();
+            }
+        }
+
+        for (AbstractCandy candy : array) { // пробегаемся по листу и выбираем самый дешевый обьект, возвращаем его
+            if (minCost == candy.getCost()) {
+                return candy;
+            }
+        }
+        return null;
+    }
+
+    public void optimizeWeight(int desirableWeight) {
+        if (desirableWeight < getOverallWeight()) {
+
+            while (desirableWeight < getOverallWeight() && getMinWeightCandy() != null) {
+                array.remove(getMinWeightCandy());
+            }
+
+            System.out.println("Successful weight optimization! Current weight: " + getOverallWeight());
+
+        } else {
+            System.out.println("Current weight already meets your request");
+        }
+    }
+
+    public void optimizeCost(int desirableCost) {
+        if (desirableCost < getOverallCost()) {
+
+            while (desirableCost < getOverallCost() && getMinCostCandy() != null) {
+                array.remove(getMinCostCandy());
+            }
+
+            System.out.println("Successful cost optimization! Current cost: " + getOverallCost());
+
+        } else {
+            System.out.println("Current cost already meets your request");
         }
     }
 }
--- src/ru/soular/ibs/homework/Main.java
+++ src/ru/soular/ibs/homework/Main.java
@@ -1,12 +1,26 @@
 package ru.soular.ibs.homework;
 
-import ru.soular.ibs.homework.candy.AbstractCandy;
+import ru.soular.ibs.homework.candy.Caramel;
 import ru.soular.ibs.homework.candy.ChocolateBar;
-
-import java.util.ArrayList;
-import java.util.List;
+import ru.soular.ibs.homework.candy.Lollipop;
 
 public class Main {
     public static void main(String[] args) {
+        CandyBox candyBox = new CandyBox();
+
+        candyBox.add(new ChocolateBar("Mars", 150, 40, true, "Milk"));
+        candyBox.add(new Caramel("Caramelly", 100, 30, true, true));
+        candyBox.add(new Lollipop("Chupa", 90, 25, true, "green"));
+        candyBox.add(new Lollipop("Chupa", 90, 25, true, "red"));
+        candyBox.add(new Lollipop("Chupa", 90, 25, true, "yellow"));
+        candyBox.add(new Lollipop("Chupa", 90, 25, true, "purple"));
+
+
+        candyBox.printOverallCost();
+        candyBox.optimizeCost(400);
+
+        candyBox.printWeight();
+        candyBox.printContents();
+
     }
 }

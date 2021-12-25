0381a07a9a3efd00813d522e5c2105550e48774d
Noire86 <gleb.iwanow2010@yandex.ru>
2021-12-26 02:33:10 +0300

    Some progress

--- src/ru/soular/ibs/homework2/util/FileHandler.java
+++ src/ru/soular/ibs/homework2/util/FileHandler.java
@@ -1,4 +1,22 @@
 package ru.soular.ibs.homework2.util;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class FileHandler {
+
+    public static List<String> parseFile(File file) throws Exception {
+        List<String> list = new ArrayList<>();
+        String[] array;
+        FileReader reader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+
+        while (bufferedReader.ready()){
+        }
+
+
+    }
 }

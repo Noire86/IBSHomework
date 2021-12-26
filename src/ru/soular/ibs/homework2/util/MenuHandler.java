a53ec9cb4f2f390c5197ba7c9b8eefd54f44ae7d
SoularX86 <gleb.iwanow2010@yandex.ru>
2021-12-26 22:40:42 +0500

    Cosmetic changes to menu

--- src/ru/soular/ibs/homework2/util/MenuHandler.java
+++ src/ru/soular/ibs/homework2/util/MenuHandler.java
@@ -5,12 +5,14 @@ import java.util.List;
 public class MenuHandler {
 
     public static void printMenu() {
-        System.out.println("Welcome to AppStatist!\n" +
+        System.out.println("======================================================\n" +
+                "Welcome to AppStatist!\n" +
                 "Use this menu to operate:\n" +
                 "1. Print all words in alphabetical order\n" +
                 "2. Print all unique words in alphabetical order\n" +
                 "3. Print the overall words count\n" +
                 "4. Print the most frequent words\n" +
-                "5. Quit application");
+                "5. Quit application\n" +
+                "======================================================");
     }
 }

838f790dc81fe32d8f3eebf37f250b7911e2f730
SoularX86 <gleb.iwanow2010@yandex.ru>
2021-12-26 21:52:56 +0500

    Implemented Menu and stats classes

---
+++ src/ru/soular/ibs/homework2/util/MenuHandler.java
@@ -0,0 +1,15 @@
+package ru.soular.ibs.homework2.util;
+
+import java.util.List;
+
+public class MenuHandler {
+
+    public static void printMenu() {
+        System.out.println("Welcome to AppStatist!\n" +
+                "Use this menu to operate:\n" +
+                "1. Print all words in alphabetical order\n" +
+                "2. Print all unique words in alphabetical order\n" +
+                "3. Print the most used words count\n" +
+                "4. Quit application");
+    }
+}

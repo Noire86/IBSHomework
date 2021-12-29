46e9be3a303940388df63ed861b9ce1912d60ffe
Noire86 <gleb.iwanow2010@yandex.ru>
2021-12-29 19:39:29 +0300

    Implemented JsonHandler

--- src/main/java/ru/soular/ibs/homework3/util/JsonHandler.java
+++ src/main/java/ru/soular/ibs/homework3/util/JsonHandler.java
@@ -1,8 +1,38 @@
 package ru.soular.ibs.homework3.util;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import ru.soular.ibs.homework3.classes.Company;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
 public class JsonHandler {
-    private Gson gson = new Gson();
+    private static final Gson gson = new GsonBuilder().create();
+
+    static class SerializableModel {
+        public List<Company> companies;
+
+        public List<Company> getCompanies() {
+            return companies;
+        }
+    }
+
+    public static <T> String serialize(T obj){
+       return gson.toJson(obj);
+    }
+
+    public static List<Company> deserialize(String s){
+        SerializableModel model = gson.fromJson(s, SerializableModel.class);
+        return model.getCompanies();
+    }
+
+
+    public static String getJSON(Path path) throws IOException {
+        return String.join("", Files.readAllLines(path)).trim();
+    }
+
 
 }

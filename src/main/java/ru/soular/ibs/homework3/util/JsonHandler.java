eb51803a06afe788a3d304de3cda0e29a79991d6
SoularX86 <gleb.iwanow2010@yandex.ru>
2021-12-30 17:14:11 +0500

    New methods in JsonHandler

--- src/main/java/ru/soular/ibs/homework3/util/JsonHandler.java
+++ src/main/java/ru/soular/ibs/homework3/util/JsonHandler.java
@@ -2,12 +2,14 @@ package ru.soular.ibs.homework3.util;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import ru.soular.ibs.homework3.classes.Company;
+import ru.soular.ibs.homework3.classes.Security;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
 
 public class JsonHandler {
     private static final Gson gson = new GsonBuilder().create();
@@ -20,15 +22,34 @@ public class JsonHandler {
         }
     }
 
-    public static <T> String serialize(T obj){
-       return gson.toJson(obj);
+    public static <T> String serialize(T obj) {
+        return gson.toJson(obj);
     }
 
-    public static List<Company> deserialize(String s){
+    public static List<Company> deserialize(String s) throws JsonSyntaxException {
         SerializableModel model = gson.fromJson(s, SerializableModel.class);
         return model.getCompanies();
     }
 
+    public static List<String> getCurrencies(List<Company> companyList) throws NullPointerException {
+        Set<String> set = new HashSet<>();
+        companyList.forEach(c -> c.getSecurities().forEach(security -> set.addAll(Arrays.asList(security.getCurrency()))));
+        return new ArrayList<>(set);
+    }
+
+    public static boolean checkCurrency(String currency, List<Company> companyList) {
+        try {
+             if (getCurrencies(companyList).stream().anyMatch(s -> s.equalsIgnoreCase(currency))) {
+                 return true;
+             } else {
+                 System.out.println("Provided currency type is not used in the file!");
+                 return false;
+             }
+        } catch (NullPointerException e) {
+            System.out.println("Currencies were not found!");
+            return false;
+        }
+    }
 
     public static String getJSON(Path path) throws IOException {
         return String.join("", Files.readAllLines(path)).trim();
